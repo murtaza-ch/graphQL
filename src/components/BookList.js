206598c8fa67aaef3ccc8dd21e840b4eaec18f50
murtaza-ch <murtazach275@gmail.com>
2020-09-08 11:48:29 -0700

    final commit

---
+++ src/components/BookList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import BookDetails from './BookDetails';
+import {getBooksQuery} from '../queries/queries';
+import {graphql} from 'react-apollo';
+
+class BookList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            selected:null
+        }
+    }
+
+    displayBooks(){
+        var data=this.props.data;
+        if (data.loading) {
+            return <div>Loading books...</div>
+        }
+        else{
+            return (
+                data.books && data.books.map((book)=>{
+                    return <li key={book.id} onClick={(e)=>this.setState({selected:book.id})}>{book.name}</li>
+                })
+            )
+        }
+    }
+
+
+    render() {
+
+        return (
+            <div>
+                
+                <ul id="book-list">
+                    {this.displayBooks()}
+                </ul>
+                <BookDetails bookId={this.state.selected}/>
+
+            </div>
+        )
+    }
+}
+
+export default graphql(getBooksQuery)(BookList);
